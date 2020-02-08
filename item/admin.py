7ee98d5275f3f77eb755a94c667574cbe5452fd6
Lahiru Samaraweera <lahiru.studixeon@gmail.com>
2020-02-08 13:30:16 +0530

    WIP

--- item/admin.py
+++ item/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import item
+from .models import item, category
 
 admin.site.register(item)
+admin.site.register(category)
--- item/models.py
+++ item/models.py
@@ -7,11 +7,32 @@ class item(models.Model, BaseModel):
     item_code = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     currency = models.CharField(max_length=10)
+    deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        'category',
+        on_delete=models.PROTECT,
+    )
 
     def toDic(self):
         return {
+            "id" : self.id,
             "name" : self.name,
             "price" : float(self.price),
             "currency" : self.currency,
-            "description" : self.description
+            "description" : self.description,
+            "is_active" : self.is_active,
+            "deleted" : self.deleted,
+            "category" : self.category.toDic()
         }
+
+class category(models.Model, BaseModel):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+
+    def toDic(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "description" : self.description
+        } 
